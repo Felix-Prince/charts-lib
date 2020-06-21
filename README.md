523050dcf90d3c982f6dfff5ac610e862f0b1606
Felix.Price <zhupengfei1995@gmail.com>
2020-06-21 21:45:36 +0800

    基础版 echarts 组件完成 能够简单渲染图表组件 具体的配置还需完善

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 > webpack 的配置说明参考 webpack.md
 
 ## 构建自己的组件库，主要是图表相关的组件
+
+第一步 基础
+
+一个 echarts 最基本的三部分就是下面三行代码
+
+-   根据一个 DOM 元素生成一个 echarts 实例
+-   设置 echarts 的属性，图表长什么样，有哪些东西显示
+-   把这些配置与实例关联起来
+
+```js
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById("main"));
+var option = {};
+myChart.setOption(option);
+```
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import ECharts from "./core/ECharts";
 
 const App: React.FC = () => {
-    return <div>Hello react</div>;
+    const [chartData, setChartData] = useState([5, 20, 36, 10, 10, 20]);
+    const lineOption: any = {
+        title: {
+            text: "ECharts 入门示例",
+        },
+        tooltip: {},
+        legend: {
+            data: ["销量"],
+        },
+        xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        },
+        yAxis: {},
+        series: [
+            {
+                name: "销量",
+                type: "bar",
+                data: chartData,
+            },
+        ],
+    };
+
+    const setData = (): void => {
+        let arr: Array<number> = [];
+        for (let index = 0; index < 6; index++) {
+            const element = Math.round(Math.random() * 100);
+            arr.push(element);
+        }
+        console.log("arr", arr);
+        setChartData(arr);
+    };
+    return (
+        <div>
+            Hello react
+            <ECharts option={lineOption} />
+            <ECharts option={lineOption} />
+            <button onClick={setData}>setData</button>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
